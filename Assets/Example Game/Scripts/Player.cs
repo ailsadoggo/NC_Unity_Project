ed61c4276e08febe89645c8da89311a846a33d20
ailsadoggo <36156937+ailsadoggo@users.noreply.github.com>
2018-02-26 11:25:31 +0000

    added movement and speed within player

--- Assets/Example Game/Scripts/Player.cs
+++ Assets/Example Game/Scripts/Player.cs
@@ -4,17 +4,23 @@ using System.Collections;
 public class Player : MonoBehaviour {
 
     public string logText = "Kill Me Now :D";
+    public float speed = 2;
 
     // Use this for initialization
     void Start () {
         
         Debug.Log(logText);
-	}
+        
+    }
 	
 	// Update is called once per frame    
 	void Update () {
 
         Debug.Log(logText);
+        Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
+        
+       rigidbody.velocity = Vector2.right *speed;
+
 
     }
 }
