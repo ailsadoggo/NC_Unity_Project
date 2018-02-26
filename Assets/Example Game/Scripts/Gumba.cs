dfd88dd085895d51a24e6c2ab80afc070efb46b2
ailsadoggo <36156937+ailsadoggo@users.noreply.github.com>
2018-02-26 12:52:02 +0000

    edited c#

---
+++ Assets/Example Game/Scripts/Gumba.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gumba : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    //Collision
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("collison!");
+}
--- Assets/Example Game/Scripts/Player.cs
+++ Assets/Example Game/Scripts/Player.cs
@@ -5,22 +5,31 @@ public class Player : MonoBehaviour {
 
     public string logText = "Kill Me Now :D";
     public float speed = 2;
+    public float health = 10;
 
     // Use this for initialization
     void Start () {
         
         Debug.Log(logText);
+        ApplyDamage(1); 
         
     }
 	
 	// Update is called once per frame    
 	void Update () {
 
-        Debug.Log(logText);
+        
         Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
         
        rigidbody.velocity = Vector2.right *speed;
 
-
     }
+
+    //Damage to mario
+    void ApplyDamage (float DamageToDeal)
+    {
+
+        health = health - DamageToDeal;
+            }
+
 }
